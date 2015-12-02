b1a7631a2598635ab1e7f4819a3feec4ed4f3c2b
Steve Kinney <hello@stevekinney.net>
2015-12-02 13:21:55 -0700

    FIXUP: Swap in jQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require javascript
-//= require javascript_ujs
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
